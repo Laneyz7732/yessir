2d75f7700abc0be291a5378219251b60aee11dc9
Laneyz7732 <59370297+Laneyz7732@users.noreply.github.com>
2020-02-01 20:52:51 -0500

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -3,9 +3,9 @@ const Discord = require("discord.js")
 exports.run = (client, message, args) => {
     const embedStats = new Discord.RichEmbed()
       .setTitle("**Help**")
-      .setFooter("Earn 5 invites to gain access to **Giveaways** and a 2 hour cooldown!")
-      .setURL("")
+      .setFooter("Why u need help command")
+      .setURL("No")
       .setColor("RED")
-      .setDescription('**\n\n  |!origin - Generates an Origin Account|!uplay - Generates a uPlay Account|!help - This Command|\n\n**    |!ban|!kick|!serverinfo');
+      .setDescription('**\n\n dmall , ban , kick , serverinfo');
     message.channel.send(embedStats);
 };
